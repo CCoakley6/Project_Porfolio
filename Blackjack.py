cd0da72d0ec187b01dcc0e7c7a9e2636d41ad0f6
CCoakley6 <48608921+CCoakley6@users.noreply.github.com>
2022-12-21 23:34:41 -0700

    Initialize Blackjack.py

---
+++ Blackjack.py
@@ -0,0 +1,7 @@
+# Simple blackjack game in Python
+
+suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
+ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Jack', 'Queen', 'King', 'Ace']
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+
+playing = True
